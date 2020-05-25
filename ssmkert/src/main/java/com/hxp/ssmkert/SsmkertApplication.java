7b3ef7efc0ded30307a92c3650644853ce9afa2f
BigFish777 <2426712259@qq.com>
2020-05-25 17:01:03 +0800

    修改测试

--- ssmkert/src/main/java/com/hxp/ssmkert/SsmkertApplication.java
+++ ssmkert/src/main/java/com/hxp/ssmkert/SsmkertApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SsmkertApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello,Welcome to SpaceSupermarket");
         SpringApplication.run(SsmkertApplication.class, args);
     }
 
