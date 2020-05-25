c8d61d28a60f14ebf2cd6c80613dcf7bc839a7e7
BigFish777 <2426712259@qq.com>
2020-05-25 16:57:49 +0800

    创建java后台项目

---
+++ ssmkert/src/main/java/com/hxp/ssmkert/SsmkertApplication.java
@@ -0,0 +1,13 @@
+package com.hxp.ssmkert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmkertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmkertApplication.class, args);
+    }
+
+}
---
+++ ssmkert/src/test/java/com/hxp/ssmkert/SsmkertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxp.ssmkert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmkertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
